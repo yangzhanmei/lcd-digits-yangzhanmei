32f2c6b9522dbe863b787462217164c1b38e11ad
fat-gril <15809244435@163.com>
2016-05-11 23:36:14 +0800

    motify

---
+++ lcd/test/main-test.js
@@ -0,0 +1,47 @@
+describe('lcd-digits', function () {
+  var inputs;
+
+  beforeEach(function () {
+    inputs = '910'
+  });
+  it('should print correct text', function () {
+
+    spyOn(console, 'log');
+
+    printReceipt(inputs);
+
+    var expectText = '\n._....._.\n'
+                      +'|_|..||.|\n'
+                      +'..|..||_|';
+
+    expect(console.log).toHaveBeenCalledWith(expectText);
+  });
+});
+
+describe('getNumberDigits', function () {
+  var numbers;
+
+  beforeEach(function () {
+     numbers = '910'
+  });
+  it('numberDigits should be this', function () {
+    var newNumberDigits = getNumberDigits(numbers);
+    var numberDigits = [
+    {values:{
+        firstLine:'._.',
+        secondLine:'|_|',
+        thirdLine:'..|'
+      }},
+    {values:{
+        firstLine:'...',
+        secondLine:'..|',
+        thirdLine:'..|'
+      }},
+    {values:{
+        firstLine:'._.',
+        secondLine:'|.|',
+        thirdLine:'|_|'
+      }}];
+    expect(newNumberDigits).toEqual(numberDigits);
+  });
+});
