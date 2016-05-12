c134ca001e742ca12d6f68eb2d026f80a918c51b
fat-gril <15809244435@163.com>
2016-05-12 16:37:03 +0800

    first cimmit

--- lcd/main/main.js
+++ lcd/main/main.js
@@ -1,56 +1,60 @@
-function printReceipt(numbers) {
-  var numberDigits = getNumberDigits(numbers);
-  var result = getResult(numberDigits);
+function printResult(numbers) {
+    var charNumbers = numbersSplit(numbers);
+    var numberDigits = getNumberDigits(charNumbers);
+    var result = getResult(numberDigits);
 
-  console.log(result);
+    console.log(result);
 }
 
-function getNumberDigits(numbers)
-{
-  var charNumbers = numbers.split('');
-  var digists = buildDigits();
-  var numberDigits = [];
-  charNumbers.forEach(function(number){
-    for(var i = 0; i < digists.length; i ++){
-      if(parseInt(number) === digists[i].key)
-      {
-        numberDigits.push({values:digists[i].values});
-      }
-    }
-  });
-  
-  return numberDigits;
+function numbersSplit(numbers) {
+    var charNumbers = numbers.split('');
+
+    return charNumbers;
+}
+
+function getNumberDigits(charNumbers) {
+    var digits = buildDigits();
+    var numberDigits = [];
+
+    charNumbers.forEach(function (number) {
+        for (var i = 0; i < digits.length; i++) {
+            if (parseInt(number) === digits[i].key) {
+                numberDigits.push({values: digits[i].values});
+            }
+        }
+    });
+
+    return numberDigits;
 }
 
 function getResult(numberDigits) {
-  var first = '';
-  var second = '';
-  var third = '';
-  var resultString = '';
+    var first = '';
+    var second = '';
+    var third = '';
+    var resultString = '';
 
-  numberDigits.forEach(function (key) {
-    first += key.values.firstLine;
-    second += key.values.secondLine;
-    third += key.values.thirdLine;
-  });
-  resultString += first + '\n' + second + '\n' + third;
+    numberDigits.forEach(function (key) {
+        first += key.values.firstLine;
+        second += key.values.secondLine;
+        third += key.values.thirdLine;
+    });
+    resultString += first + '\n' + second + '\n' + third;
 
-  return '\n'+resultString;
+    return '\n' + resultString;
 }
 
 function buildDigits() {
-  var digist = [];
 
-  digist.push({key:0,values:{firstLine:'._.',secondLine:'|.|',thirdLine:'|_|'}});
-  digist.push({key:1,values:{firstLine:'...',secondLine:'..|',thirdLine:'..|'}});
-  digist.push({key:2,values:{firstLine:'._.',secondLine:'._|',thirdLine:'|_.'}});
-  digist.push({key:3,values:{firstLine:'._.',secondLine:'._|',thirdLine:'._|'}});
-  digist.push({key:4,values:{firstLine:'...',secondLine:'|_|',thirdLine:'..|'}});
-  digist.push({key:5,values:{firstLine:'._.',secondLine:'|_.',thirdLine:'._|'}});
-  digist.push({key:6,values:{firstLine:'._.',secondLine:'|_.',thirdLine:'|_|'}});
-  digist.push({key:7,values:{firstLine:'._.',secondLine:'..|',thirdLine:'..|'}});
-  digist.push({key:8,values:{firstLine:'._.',secondLine:'|_|',thirdLine:'|_|'}});
-  digist.push({key:9,values:{firstLine:'._.',secondLine:'|_|',thirdLine:'..|'}});
-  
-  return digist;
+    return [
+        {key: 0, values: {firstLine: '._.', secondLine: '|.|', thirdLine: '|_|'}},
+        {key: 1, values: {firstLine: '...', secondLine: '..|', thirdLine: '..|'}},
+        {key: 2, values: {firstLine: '._.', secondLine: '._|', thirdLine: '|_.'}},
+        {key: 3, values: {firstLine: '._.', secondLine: '._|', thirdLine: '._|'}},
+        {key: 4, values: {firstLine: '...', secondLine: '|_|', thirdLine: '..|'}},
+        {key: 5, values: {firstLine: '._.', secondLine: '|_.', thirdLine: '._|'}},
+        {key: 6, values: {firstLine: '._.', secondLine: '|_.', thirdLine: '|_|'}},
+        {key: 7, values: {firstLine: '._.', secondLine: '..|', thirdLine: '..|'}},
+        {key: 8, values: {firstLine: '._.', secondLine: '|_|', thirdLine: '|_|'}},
+        {key: 9, values: {firstLine: '._.', secondLine: '|_|', thirdLine: '..|'}}
+    ];
 }
--- lcd/test/main-test.js
+++ lcd/test/main-test.js
@@ -1,47 +1,68 @@
 describe('lcd-digits', function () {
-  var inputs;
+    var inputs;
 
-  beforeEach(function () {
-    inputs = '910'
-  });
-  it('should print correct text', function () {
+    beforeEach(function () {
+        inputs = '910'
+    });
+    it('should print correct text', function () {
 
-    spyOn(console, 'log');
+        spyOn(console, 'log');
 
-    printReceipt(inputs);
+        printResult(inputs);
 
-    var expectText = '\n._....._.\n'
-                      +'|_|..||.|\n'
-                      +'..|..||_|';
+        var expectText = '\n._....._.\n'
+            + '|_|..||.|\n'
+            + '..|..||_|';
 
-    expect(console.log).toHaveBeenCalledWith(expectText);
-  });
+        expect(console.log).toHaveBeenCalledWith(expectText);
+    });
+});
+
+describe('numbersSplit', function () {
+    var numbers;
+
+    beforeEach(function () {
+        numbers = '910'
+    });
+    it('return correct charNumbers', function () {
+        var newCharNumbers = numbersSplit(numbers);
+        var charNumbers = ['9', '1', '0'];
+
+        expect(newCharNumbers).toEqual(charNumbers);
+    });
 });
 
 describe('getNumberDigits', function () {
-  var numbers;
+    var charNumbers;
 
-  beforeEach(function () {
-     numbers = '910'
-  });
-  it('numberDigits should be this', function () {
-    var newNumberDigits = getNumberDigits(numbers);
-    var numberDigits = [
-    {values:{
-        firstLine:'._.',
-        secondLine:'|_|',
-        thirdLine:'..|'
-      }},
-    {values:{
-        firstLine:'...',
-        secondLine:'..|',
-        thirdLine:'..|'
-      }},
-    {values:{
-        firstLine:'._.',
-        secondLine:'|.|',
-        thirdLine:'|_|'
-      }}];
-    expect(newNumberDigits).toEqual(numberDigits);
-  });
+    beforeEach(function () {
+        charNumbers = ['9', '1', '0']
+    });
+    it('return correct numberDigits', function () {
+        var newNumberDigits = getNumberDigits(charNumbers);
+        var numberDigits = [
+            {
+                values: {
+                    firstLine: '._.',
+                    secondLine: '|_|',
+                    thirdLine: '..|'
+                }
+            },
+            {
+                values: {
+                    firstLine: '...',
+                    secondLine: '..|',
+                    thirdLine: '..|'
+                }
+            },
+            {
+                values: {
+                    firstLine: '._.',
+                    secondLine: '|.|',
+                    thirdLine: '|_|'
+                }
+            }];
+
+        expect(newNumberDigits).toEqual(numberDigits);
+    });
 });
